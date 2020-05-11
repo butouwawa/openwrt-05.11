9045e3bc2b35138ec8d7fefa6dd56e727877140f
butouwawa <32016942+butouwawa@users.noreply.github.com>
2020-05-11 14:43:42 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -11,5 +11,8 @@
 # Uncomment a feed source
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
+# 群里看到添加SSR方法
+sed -i 's/#src-git helloworld/src-git helloworld/g' feeds.conf.default
+
 # Add a feed source
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
